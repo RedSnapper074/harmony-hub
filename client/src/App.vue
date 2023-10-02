dce7cca4944d2ffd965f1ae6bab11afbfcfc7829
RedSnapper074 <karthikguntuka074@gmail.com>
2023-10-02 20:57:40 +0530

    init the vue js application using vue cli

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -11,13 +10,58 @@ export default {
 }
 </script>
 
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e40;
+    background-color: #f0f0f0;
+}
+
+.register {
+    width: 100%;
+    background-color: #292929;
+}
+
+.register img {
+    margin-top: 10px;
+    width: 200px;
+    background-color: green;
+    border-radius: 50px;
+    padding: 10px;
+}
+
+.register input{
+    width: 400px;
+    height: 40px;
+    padding-left: 10px;
+    display: block;
+    margin-bottom: 20px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid green;
+    border-radius: 20px;
+}
+
+.register button {
+    width: 320px;
+    padding: 10px;
+    background: lightgreen;
+    border-radius: 50px;
+    border: 1px solid green;
+    cursor: pointer; 
+    margin-bottom: 30px
+}
+
+h1 {
+    color: green;
+    font-size: 60px;
 }
 </style>
---
+++ client/src/components/Register.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="register">
+        <img class="logo" src="../assets/bane.jpg" />
+        <h1>Welcome to HarmonyHub</h1>
+        <input type="email" v-model="email" placeholder="Enter Email" >
+        <input type="password" v-model="password" placeholder="Enter Password" >
+        <button @click="register">Register</button>
+    </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+    name: 'Register',
+    data()
+    {
+        return {
+            email:'',
+            password:''
+        }
+    },
+    
+    methods: {
+        async register(){
+            const response = await AuthenticationService.register({
+                email: this.email,
+                password: this.password
+            })
+            console.log(response.data)
+        }
+    }
+    
+}
+</script>
